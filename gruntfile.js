4c17b11909fcfa0d16ca550a969b11a68a568969
Nikooooh <nicolastrevis@hotmail.com>
2024-02-21 15:48:45 -0300

    Utilizando Grunt com JS e Less

--- gruntfile.js
+++ gruntfile.js
@@ -1,5 +1,26 @@
 module.exports = function (grunt) {
   grunt.initConfig({
-    pkg: grunt.file.readJSON("package.json"),
+    less: {
+      development: {
+        options: {
+          paths: ["less"],
+        },
+        files: {
+          "css/styles.css": "less/styles.less",
+        },
+      },
+    },
+
+    uglify: {
+      my_target: {
+        files: {
+          "js/scripts.min.js": ["js/*.js"],
+        },
+      },
+    },
   });
+
+  grunt.loadNpmTasks("grunt-contrib-less");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.registerTask("default", ["less", "uglify"]);
 };
